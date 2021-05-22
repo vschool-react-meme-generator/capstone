b7b9e677f2d18b2b3f251049cd18003b849affd0
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-05-22 12:35:34 -0600

    first commit basic html shell

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+function App() {
+    return(
+        <div className="master">
+            <div className="left-container">
+                {/* Roy's meme generator component goes here */}
+            </div>
+            <div className="right-container">
+                {/* Diogo's meme widgets components will go here */}
+            </div>           
+        </div>
+
+    )
+}
+
+export default App
