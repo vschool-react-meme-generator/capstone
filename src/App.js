7970e59e03f35b1d1daea1280bbde23b7bb672c7
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-05-26 02:21:58 -0600

    edit button still needs to be finished

--- src/App.js
+++ src/App.js
@@ -1,27 +1,120 @@
-import React from "react"
+import React, {Component} from "react"
 import MemeWidget from "./MemeWidget"
+import MemeGenerator from "./MemeGenerator"
+import axios from "axios"
 
 
-function App() {
-    return(
-        <div className="master">
-            <div className="left-container">
-                <div className="meme-generator">
-                    <p>Roy's Meme Generator Component will go here</p> 
-                    {/* Roy's meme generator component goes here */}
+class App extends Component {
+    constructor() {
+        super()
+        this.state={
+            id:"",
+            randomImage:"",
+            topText:"",
+            bottomText:"",
+            memes:[],
+            savedMemes:[]
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.deleteSavedMeme = this.deleteSavedMeme.bind(this)
+        this.editSavedMeme = this.editSavedMeme.bind(this)
+    }
+
+    deleteSavedMeme(e) {
+        const {id} = e.target
+
+        console.log(`Removing ${id}`)
+
+        let savedMemesAfterDelete = this.state.savedMemes
+        savedMemesAfterDelete.splice(id, 1)
+
+        this.setState({
+            savedMemes: savedMemesAfterDelete
+        })
+
+    }
+
+    editSavedMeme(e) {
+        const {id} = e.target
+        console.log(`we are editing ${id}`)
+
+        // needs to update the savedMemes state property for the specific meme object item in the array that is being edited. 
+
+        // within that specific meme that is being edited, there is a property called editModeOn that is set to false by default
+
+        //this method of editSavedMeme() needs to flip that to true
+
+        //Then, in the MemeWidget component, we can use a conditional rendering to evaluate wheter the component is in edit mode or not
+
+    }
+
+    //initial ret request for meme
+
+    componentDidMount(){
+        axios.get("https://api.imgflip.com/get_memes")
+            .then(res => this.setState({
+                memes: res.data.data.memes,
+                randomImage: res.data.data.memes[Math.ceil(Math.random() * res.data.data.memes.length)].url
+            }))
+            //.then(res => console.log(res.data.data.memes))
+
+            .catch(err => console.log(err)) 
+            
+    }
+
+    handleChange(event){
+        const {name,value} = event.target
+        this.setState({[name]:value})
+
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        //const randomNumber = Math.random() * this.state.memes.length
+       // const randomMemeImage = this.state.memes[randomNumber].url
+
+        this.setState(prevState => {
+           // console.log(prevState)
+            return {
+                savedMemes: [...prevState.savedMemes, {editModeOn: false,topText: prevState.topText, bottomText: prevState.bottomText, imgUrl: prevState.randomImage}],
+                randomImage: this.state.memes[Math.ceil(Math.random() * this.state.memes.length)].url,
+                topText: "",
+                bottomText: ""
+            }
+        })
+
+        console.log(this.state.savedMemes)
+    }
+
+
+
+
+    render() {
+
+        const allSavedMemes = this.state.savedMemes.map((meme, i) => <MemeWidget key={i} id={i} toptext={meme.topText} bottomtext={meme.bottomText} image={meme.imgUrl} delete={this.deleteSavedMeme} edit={this.editSavedMeme} editMode={meme.editModeOn}/> )
+
+         //console.log("State Saved Meme" + this.state.savedMemes[0].name)
+        /* console.log(this.state) */
+
+        return(
+            <div className="master">
+                <div className="left-container">
+                    <MemeGenerator randomImage={this.state.randomImage} topText={this.state.topText} bottomText={this.state.bottomText} submitForm={this.handleSubmit} changeHandler={this.handleChange} />
+                        
+                    
+                    
                 </div>
-                
+                <div className="right-container">
+                    {allSavedMemes}
+                </div>           
             </div>
-            <div className="right-container">
-                {/* all of these components below are hardcoded right now for visual reference only. They will only exist after someone submits the meme from the meme generator */}
-                <MemeWidget />  
-                <MemeWidget /> 
-                <MemeWidget /> 
-                <MemeWidget />        
-            </div>           
-        </div>
+    
+        )
 
-    )
+    }
 }
+    
+
 
 export default App
--- src/MemeWidget.js
+++ src/MemeWidget.js
@@ -1,15 +1,43 @@
 import React from "react"
 
-function MemeWidget() {
-    return (
-        <div className="meme-widgets">
-            <img className="widget-image" src="https://filmdaily.co/wp-content/uploads/2020/09/DirtyFunnyMemes-lede72-1300x1115.jpg" />
-            <div className="widget-buttons">
-                <button>Edit</button>
-                <button>Delete</button>
-            </div>
-        </div>
-    )
+function MemeWidget(props) {
+
+    if(props.editMode === false) {
+        return(
+            <div className="meme-widgets">
+                    <div className="saved-meme-widget">
+                        <img className="widget-image" src={props.image} />
+                        <input className="saved-meme-toptext" placeholder={props.toptext} ></input>
+                        <input className="saved-meme-bottomtext" placeholder={props.toptext} ></input>
+                        
+                    </div>
+                    
+                    <div className="widget-buttons">
+                        <button onClick={props.edit} id={props.id}>Save</button>
+                    </div>
+                </div>
+        )
+        
+
+    } else {
+        return (
+
+                <div className="meme-widgets">
+                    <div className="saved-meme-widget">
+                        <img className="widget-image" src={props.image} />
+                        <h3 className="saved-meme-toptext">{props.toptext}</h3>
+                        <h3 className="saved-meme-bottomtext">{props.bottomtext}</h3>
+                    </div>
+                    
+                    <div className="widget-buttons">
+                        <button onClick={props.edit} id={props.id}>Edit</button>
+                        <button onClick={props.delete} id={props.id}>Delete</button>
+                    </div>
+                </div>
+        )
+    }
+
+    
 }
 
 export default MemeWidget
